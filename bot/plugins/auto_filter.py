75529ea2b1ed11f25c44d0ed458662e0eb68a69e
AlwinMicheal <90100045+AlwinMicheal@users.noreply.github.com>
2021-09-06 17:43:25 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -184,13 +184,13 @@ async def auto_filter(bot: Client, update: Message):
                 if ((len(ibuttons)%2) == 0):
                     ibuttons.append(
                         [
-                            InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                            InlineKeyboardButton(f"💫 {Group} 💫", url=https://t.me/movies_hut_chat)
                         ]
                     )
 
                 else:
                     ibuttons[-1].append(
-                        InlineKeyboardButton(f"⚜ {Join In My Channel} ⚜", url=https://t.me/new_movies_HD_movies)
+                        InlineKeyboardButton(f"💫 {Channel} 💫", url=https://t.me/new_movies_HD_movies)
                     )
                 
             for x in ibuttons:
